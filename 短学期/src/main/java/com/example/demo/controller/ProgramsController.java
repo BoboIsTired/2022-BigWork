071c28a034c3857b2227cfffd19647b904412e13
BoboIsTired <2630756103@qq.com>
2022-07-01 15:16:58 +0800

    节目programs基本功能

--- 短学期/src/main/java/com/example/demo/controller/ProgramsController.java
+++ 短学期/src/main/java/com/example/demo/controller/ProgramsController.java
@@ -1,9 +1,18 @@
 package com.example.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.demo.common.Result;
+import com.example.demo.entity.Device;
+import com.example.demo.entity.Programs;
+import com.example.demo.mapper.ProgramsMapper;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +26,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/programs")
 public class ProgramsController {
 
+    @Resource
+    ProgramsMapper programsMapper;
+
+    //新增
+    @PostMapping
+    public Result<?> save(@RequestBody Programs programs){
+        programs.setUpdateDate(LocalDateTime.now());
+        programsMapper.insert(programs);
+        return Result.success();
+    }
+
+    //获取or查询
+    @GetMapping
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                              @RequestParam (defaultValue = "10") Integer pageSize,
+                              @RequestParam (defaultValue = "") String search){
+        LambdaQueryWrapper<Programs> wrapper =  Wrappers.<Programs>lambdaQuery();
+        if (StrUtil.isNotBlank(search)){
+            wrapper.like(Programs::getProgramName,search);
+        }
+        Page<Programs> programsPage = (Page<Programs>) programsMapper.selectPage(new Page<>(pageNum,pageSize),wrapper);
+
+        return Result.success(programsPage);
+    }
+    //修改
+    @PutMapping("/update")
+    public Result<?> update(@RequestBody Programs programs){
+        programs.setUpdateDate(LocalDateTime.now());
+        programsMapper.updateById(programs);
+        return Result.success();
+    }
 }
